6155ec98538c28d17ff15673d5164b6b0233cc77
Michael <bjmmtin@gmail.com>
2024-10-13 06:52:20 -0700

    add about module and navigate on the header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,11 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent,
     },
+    {
+        path: 'about-us',
+        loadChildren: async ()=>{
+            const m = await import('./modules/about-us/about-us-routing.module');
+            return m.AboutUsRoutingModule;
+        }
+    }
 ];
